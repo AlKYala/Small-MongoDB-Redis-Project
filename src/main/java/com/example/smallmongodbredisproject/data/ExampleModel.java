2574b850edcade7ee7d6011a10ed13d98e94a1be
Ali Yalama <aliyalama@googlemail.com>
2023-03-24 00:28:43 +0100

    FIx Redis port in config Make Document serializable so redis can save it as value reate Cache Config to overwrite Cache bean in ApplicationContext - Redis is used Modify Repo so it uses Redis

--- src/main/java/com/example/smallmongodbredisproject/data/ExampleModel.java
+++ src/main/java/com/example/smallmongodbredisproject/data/ExampleModel.java
@@ -6,16 +6,20 @@ import lombok.*;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.io.Serializable;
+
 /**
  * Remember: Collections are the equivalent to Tables
  * Doucments are the equivalent to rows
+ *
+ * Serializable so Redis can save it
  */
 
 @Document(collection = "customers")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class ExampleModel {
+public class ExampleModel implements Serializable {
     @Id
     private String id;
     private String name;
