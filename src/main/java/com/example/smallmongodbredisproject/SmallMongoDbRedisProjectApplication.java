222ee4612478a3360f7214c13ce55a65679e18d3
Ali Yalama <aliyalama@googlemail.com>
2023-03-23 21:47:52 +0100

    Init

---
+++ src/main/java/com/example/smallmongodbredisproject/SmallMongoDbRedisProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.smallmongodbredisproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmallMongoDbRedisProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmallMongoDbRedisProjectApplication.class, args);
+    }
+
+}
