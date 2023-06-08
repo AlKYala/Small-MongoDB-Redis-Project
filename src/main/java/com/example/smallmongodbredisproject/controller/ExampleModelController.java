bb0590811290fb88106ffcdd1d01431558767894
Ali Yalama <aliyalama@googlemail.com>
2023-06-08 14:42:51 +0200

    logs on controller

--- src/main/java/com/example/smallmongodbredisproject/controller/ExampleModelController.java
+++ src/main/java/com/example/smallmongodbredisproject/controller/ExampleModelController.java
@@ -2,11 +2,15 @@ package com.example.smallmongodbredisproject.controller;
 
 import com.example.smallmongodbredisproject.data.ExampleModel;
 import com.example.smallmongodbredisproject.service.ExampleService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/example")
+@Slf4j
 public class ExampleModelController {
 
     @Autowired
@@ -14,11 +18,19 @@ public class ExampleModelController {
 
     @PostMapping("/create")
     private ExampleModel create(@RequestBody ExampleModel model) {
+        log.info("RECEIVE CREATE CALL");
         return this.exampleService.create(model);
     }
 
     @GetMapping("/{id}")
     private ExampleModel findById(@PathVariable String id) {
+        log.info("RECEIVE FIND BY ID CALL");
         return this.exampleService.findById(id);
+    }
+
+    @GetMapping()
+    private List<ExampleModel> findAll() {
+        log.info("RECEIVE FIND ALL CALL");
+        return this.exampleService.findAll();
     }
 }
